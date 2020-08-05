a29b1c0681afe844cbd86053f50d2fbf9fe24430
Devin <991476256@qq.com>
2020-08-05 17:15:07 +0800

    init project

---
+++ src/main/java/com/oocl/todoListEnd/TodoListEndApplication.java
@@ -0,0 +1,13 @@
+package com.oocl.todoListEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoListEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oocl/todoListEnd/TodoListEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oocl.todoListEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
