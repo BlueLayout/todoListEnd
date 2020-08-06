633aa1a8356be78c8184567b8f3d7d68d208d19e
Devin <991476256@qq.com>
2020-08-07 02:02:16 +0800

    [todolist][devin]feature: implement queryTodos in TodoService

--- src/main/java/com/oocl/todoListEnd/service/Impl/TodoServiceImpl.java
+++ src/main/java/com/oocl/todoListEnd/service/Impl/TodoServiceImpl.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TodoServiceImpl implements TodoService {
@@ -29,5 +31,11 @@ public class TodoServiceImpl implements TodoService {
     @Transactional
     public void deleteTodo(String id) {
         todoRepository.deleteById(id);
+    }
+
+    @Override
+    public List<TodoResponse> queryTodos() {
+        return todoRepository.findAll().stream()
+                .map(todo -> todoMapper.todoToTodoResponse(todo)).collect(Collectors.toList());
     }
 }
--- src/main/java/com/oocl/todoListEnd/service/TodoService.java
+++ src/main/java/com/oocl/todoListEnd/service/TodoService.java
@@ -3,9 +3,13 @@ package com.oocl.todoListEnd.service;
 import com.oocl.todoListEnd.model.dto.TodoResponse;
 import com.oocl.todoListEnd.model.entity.Todo;
 
+import java.util.List;
+
 public interface TodoService {
 
     TodoResponse createTodo(Todo todo);
 
     void deleteTodo(String id);
+
+    List<TodoResponse> queryTodos();
 }
