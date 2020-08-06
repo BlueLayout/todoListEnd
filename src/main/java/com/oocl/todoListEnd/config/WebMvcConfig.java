912dc40e5a6722582cce08ebf6c9baa67b7183e7
Devin <991476256@qq.com>
2020-08-07 03:13:56 +0800

    [todolist][devin]feature: update WebMvcConfig to Solve cross-domain problems

--- src/main/java/com/oocl/todoListEnd/config/WebMvcConfig.java
+++ src/main/java/com/oocl/todoListEnd/config/WebMvcConfig.java
@@ -8,10 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("http://localhost:3000")
+        registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
                 .maxAge(3600)
                 .allowCredentials(true);
     }
+
 }
