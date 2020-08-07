cdfb6621dc08edc4fd5336f8396b1961f70ac8f5
Devin <991476256@qq.com>
2020-08-07 08:38:53 +0800

    [todolist][devin]bugfix: replace NullPointException to NoTodoDataException

--- src/main/java/com/oocl/todoListEnd/config/GlobalExceptionHandler.java
+++ src/main/java/com/oocl/todoListEnd/config/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.oocl.todoListEnd.config;
 
+import com.oocl.todoListEnd.exception.NoTodoDataException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler({IllegalArgumentException.class,NullPointerException.class})
+    @ExceptionHandler({IllegalArgumentException.class, NoTodoDataException.class})
     @ResponseStatus(HttpStatus.FORBIDDEN)
     String handleIllegalOperationException(Exception e) {
         return e.getMessage();
